700c03fdbd7ebb5bf6ee0b9344c20507347d1f93
PCCOPA <panomphen865@gmail.com>
2026-05-05 13:35:22 +0700

    fix: สร้าง Resend instance ใน sendEmail แทนที่จะสร้างตอน module load

--- config/email.js
+++ config/email.js
@@ -1,12 +1,12 @@
 const { Resend } = require('resend');
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 const sendEmail = async ({ to, subject, html }) => {
   if (!process.env.RESEND_API_KEY) {
     console.warn('Email skipped: RESEND_API_KEY not set');
     return;
   }
+
+  const resend = new Resend(process.env.RESEND_API_KEY);
 
   await resend.emails.send({
     from   : 'Accdee <onboarding@resend.dev>',
